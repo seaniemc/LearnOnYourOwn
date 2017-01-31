0717cda5c925484e54e2f875f0f601e31c4cbf8d
GARY MC HUGH <G00308668@gmit.ie>
2017-01-31 12:32:33 +0000

    Updated Project

--- src/main/resources/static/app/services/user.service.js
+++ src/main/resources/static/app/services/user.service.js
@@ -2,19 +2,19 @@
 todoApp.factory('Users', ['$http', function($http) {
 	return {
 		get: function() {
-			return $http.get('/api/todos');
+			return $http.get('/api/users');
 		},
 
 		create: function(todoData) {
-			return $http.post('/api/todos', todoData);
+			return $http.post('/api/users', todoData);
 		},
 
 		delete: function(id) {
-			return $http.delete('/api/todos/' + id);
+			return $http.delete('/api/users/' + id);
 		},
 
 		update: function(todoData) {
-			return $http.put('/api/todos/' + todoData.id, todoData);
+			return $http.put('/api/users/' + todoData.id, todoData);
 		}
 	}
 }]);
