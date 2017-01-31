5199d6c2244a9b13e0304822aa6b99fc74d4680c
GARY MC HUGH <G00308668@gmit.ie>
2017-01-31 12:12:49 +0000

    added Users

---
+++ src/main/java/todoapp/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package todoapp.repositories;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import todoapp.models.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+	public List<User> findAll();
+	public User findOne(String id);
+	public User save(User user);
+	public void delete(User user);
+}
